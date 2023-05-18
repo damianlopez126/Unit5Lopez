a1d53684e30636c544e3d51cd81ca5d02f0d4cd7
damianlopez126 <731765@student.dusd.net>
2023-05-18 15:17:38 -0700

    deindf
    wfwefwef

---
+++ Unit5LopezP1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoretext;
+    public TextMeshProUGUI gameOverText;
+    private int score;
+    private float spawnRate = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+
+        gameOverText.gameObject.SetActive(true);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+
+            
+        }
+       
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoretext.text = "Score" + score;
+    }
+}
